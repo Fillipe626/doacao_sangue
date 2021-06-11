b44c804e9605e89d739140e76f7a6c2454f5b6fd
Fillipe626 <lpjp91@gmail.com>
2021-06-11 11:10:55 -0400

    Doação registrada na coleção do usuário com sucesso o/

--- lib/screens/donation_screen.dart
+++ lib/screens/donation_screen.dart
@@ -19,93 +19,80 @@ class _DonationScreenState extends State<DonationScreen> {
   final _scaffoldKey = GlobalKey<
       ScaffoldState>(); //Chave global para acesso do scafold nas funções on success e on fail
   final _formKey =
-  GlobalKey<FormState>(); //Chave global para acesso do formulário
+      GlobalKey<FormState>(); //Chave global para acesso do formulário
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
+        key: _scaffoldKey,
+        body: ScopedModelDescendant<DonationModel>(
+            builder: (context, child, model) {
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    DataInputFormatter(),
+                  ],
+                  controller: _dateController,
+                  decoration: InputDecoration(hintText: "Data da doação"),
+                  validator: (text) {
+                    if (text.isEmpty) return "Informe a data da sua doação!";
+                  },
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                      child: Text(
+                        "Registrar doação",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        if (UserModel.of(context).isLoggedIn()) {
+                          DonationData donationData = DonationData();
+                          donationData.data = _dateController.text;
 
-      body: ScopedModelDescendant<DonationModel>(
-          builder: (context, child, model) {
-            return Form(
-                key: _formKey,
-                child: ListView(
-                padding: EdgeInsets.all(16.0),
-            children: <Widget>[
-            TextFormField(
-            keyboardType: TextInputType.phone,
-            inputFormatters: [
-            FilteringTextInputFormatter.digitsOnly,
-            DataInputFormatter(),
-            ],
-            controller: _dateController,
-            decoration: InputDecoration(hintText: "Data da doação"),
-            validator: (text) {
-            if (text.isEmpty) return "Informe a data da sua doação!";
-            },
+                          DonationModel.of(context).addDonation(donationData);
+                        } else {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                        }
+                      }
+                      ),
+                ),
+              ],
             ),
-            SizedBox(
-            height: 44.0,
-            child: RaisedButton(
-            child: Text(
-            "Registrar doação",
-            style: TextStyle(
-            fontSize: 18.0,
-            ),
-            ),
-            textColor: Colors.white,
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-            if (UserModel.of(context).isLoggedIn()) {
-
-            DonationData donationData = DonationData();
-            donationData.data = _dateController.text;
-
-            DonationModel.of(context).addDonation(donationData);
-            }else { Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginScreen())
-            );
-            }
-            } : null,
-              child: Text(UserModel.of(context).isLoggedIn()))
-
-          }
-
-      ),
-    )
-    ,
-    ]
-    ,
-    )
-    ,
+          );
+        }
+      )
     );
   }
 
-  ,
+  void _onSuccess() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Doação registrada com sucesso!"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 3),
+    ));
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      //Depois do usuário criado, aguardamos dois segundos e damos um pop na tela para retornar
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
 
-  )
-
-  );
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Falha ao registrar a doação!"),
+      backgroundColor: Colors.redAccent,
+      duration: Duration(seconds: 5),
+    ));
+  }
 }
-
-void _onSuccess() {
-  _scaffoldKey.currentState.showSnackBar(SnackBar(
-    content: Text("Doação registrada com sucesso!"),
-    backgroundColor: Theme
-        .of(context)
-        .primaryColor,
-    duration: Duration(seconds: 3),
-  ));
-  Future.delayed(Duration(seconds: 2)).then((_) {
-    //Depois do usuário criado, aguardamos dois segundos e damos um pop na tela para retornar
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => HomeScreen()));
-  });
-}
-
-void _onFail() {
-  _scaffoldKey.currentState.showSnackBar(SnackBar(
-    content: Text("Falha ao registrar a doação!"),
-    backgroundColor: Colors.redAccent,
-    duration: Duration(seconds: 5),
-  ));
-}}
